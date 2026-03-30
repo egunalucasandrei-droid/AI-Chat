961cbdcb6c3d69942d58d2d67d0d9b10a430cc55
Lucas Eguna <egunalucasandrei@gmail.com>
2026-03-30 14:30:08 +0800

    Fix Vercel white screen

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/AI-Chat/',
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  // ADD THIS SERVER PROXY:
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  ]
 })
